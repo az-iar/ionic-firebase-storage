7543de4969d6492e7810c53aea08feb86f26f7a9
Azri Ismail <azri.ismail@nazrol.tech>
2017-10-11 15:48:10 +0800

    add example for upload file using blob

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { FirebaseProvider } from '../providers/firebase/firebase';
+import { MyApp } from "./app.component";
+import { HomePage } from "../pages/home/home";
+import { FirebaseProvider } from "../providers/firebase/firebase";
+import { HttpModule } from "@angular/http";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, HttpModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
     FirebaseProvider
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,7 +4,8 @@ import { Entry, File } from "@ionic-native/file";
 import { FileChooser } from "@ionic-native/file-chooser";
 import { FirebaseProvider } from "../../providers/firebase/firebase";
 import { FilePath } from "@ionic-native/file-path";
-import { Camera, DestinationType } from "@ionic-native/camera";
+import { Camera } from "@ionic-native/camera";
+import { Http } from "@angular/http";
 import * as path from "path";
 
 @Component({
@@ -15,18 +16,18 @@ import * as path from "path";
 export class HomePage {
   imageUri: string = "";
 
-  constructor (
+  constructor(
     public navCtrl: NavController,
     public alertCtrl: AlertController,
     public file: File,
     public filePath: FilePath,
     public fileChooser: FileChooser,
     public firebase: FirebaseProvider,
-    public camera: Camera
-  ) {
-  }
+    public camera: Camera,
+    public http: Http
+  ) {}
 
-  selectFile () {
+  selectFile() {
     // trigger file chooser
     this.fileChooser
       .open()
@@ -39,8 +40,9 @@ export class HomePage {
       });
   }
 
-  takePicture () {
-    this.camera.getPicture({
+  takePicture() {
+    this.camera
+      .getPicture({
         quality: 80
       })
       .then(uri => this.handlePreviewAndResolveFileUrl(uri))
@@ -48,7 +50,7 @@ export class HomePage {
       .catch(error => console.log(error));
   }
 
-  handlePreviewAndResolveFileUrl (filePath) {
+  handlePreviewAndResolveFileUrl(filePath) {
     // set image uri to preview image
     this.imageUri = filePath;
 
@@ -56,18 +58,35 @@ export class HomePage {
     return this.file.resolveLocalFilesystemUrl(filePath);
   }
 
-  handleFileUpload (file: Entry) {
+  handleFileUpload(file: Entry) {
     const dirname = path.dirname(file.nativeURL);
     const filename = file.name;
 
+    // For Http API, file should be converted to blob
+    // this.file.readAsArrayBuffer(dirname, filename).then(ab => {
+    //   let blob = new Blob([new Uint8Array(ab)]);
+
+    //   let formData = new FormData();
+    //   formData.append('file', blob);
+
+    //   this.http.post('/upload', formData).map(response => response.json()).subscribe(response => {
+
+    //   });
+    // })
+
     // convert file as data_url
-    return this.file.readAsDataURL(dirname, filename).then(dataUrl => {
-      // upload to firebase
-      return this.firebase.uploadFile(dataUrl, filename);
-    }).then(snapshot => {
-      return this.alertCtrl.create({
-        message: "File uploaded to Firebase!"
-      }).present();
-    });
+    return this.file
+      .readAsDataURL(dirname, filename)
+      .then(dataUrl => {
+        // upload to firebase
+        return this.firebase.uploadFile(dataUrl, filename);
+      })
+      .then(snapshot => {
+        return this.alertCtrl
+          .create({
+            message: "File uploaded to Firebase!"
+          })
+          .present();
+      });
   }
 }
